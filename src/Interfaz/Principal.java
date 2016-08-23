e663f030a4384f382f8c6311252720b9d33a335f
lenninlara28 <lenninlara28@hotmail.com>
2016-08-22 20:01:35 -0500

    Panel terminado

--- src/Interfaz/Principal.java
+++ src/Interfaz/Principal.java
@@ -31,6 +31,18 @@ public class Principal extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtSueldo = new javax.swing.JTextField();
+        cmbEjecutar = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtdescuento1 = new javax.swing.JTextField();
+        txtDescuento4 = new javax.swing.JTextField();
+        txtDescuento3 = new javax.swing.JTextField();
+        txtdescuento2 = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        txtTotal = new javax.swing.JTextField();
+        cmbNuevo = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,6 +57,50 @@ public class Principal extends javax.swing.JFrame {
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, -1, -1));
         jPanel1.add(txtSueldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(139, 50, 120, -1));
 
+        cmbEjecutar.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        cmbEjecutar.setText("Consultar");
+        cmbEjecutar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbEjecutarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cmbEjecutar, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 90, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel3.setText("Descuento Por Ley de Politica Pública ");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, 210, -1));
+
+        jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel4.setText("Descuento Por Seguro Social");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 180, -1));
+
+        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel5.setText("Descuento Por Seguro Forzoso ");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 170, -1));
+
+        jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel6.setText("Descuento Por  Caja De Ahorro ");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, -1, -1));
+        jPanel1.add(txtdescuento1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, 120, -1));
+        jPanel1.add(txtDescuento4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 300, 120, -1));
+
+        txtDescuento3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtDescuento3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtDescuento3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, 120, -1));
+        jPanel1.add(txtdescuento2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 120, -1));
+
+        jLabel7.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel7.setText("Sueldo Total ");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 200, -1, -1));
+        jPanel1.add(txtTotal, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 230, 100, -1));
+
+        cmbNuevo.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        cmbNuevo.setText("Nueva Consulta");
+        jPanel1.add(cmbNuevo, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 300, -1, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -58,6 +114,24 @@ public class Principal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cmbEjecutarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbEjecutarActionPerformed
+        String descuento1,descuento2,descuento3,descuento4,montoT;
+        int sueldo,descu1,descu2,descu4,Total;
+        sueldo=Integer.parseInt(txtSueldo.getText());
+        descu1=(sueldo/100);
+        descu2=((sueldo*4)/100);
+        double descu3=((sueldo*0.5)/100);
+        descu4=((sueldo*5)/100);
+        
+        
+        
+     
+    }//GEN-LAST:event_cmbEjecutarActionPerformed
+
+    private void txtDescuento3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDescuento3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtDescuento3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -95,9 +169,21 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmbEjecutar;
+    private javax.swing.JToggleButton cmbNuevo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtDescuento3;
+    private javax.swing.JTextField txtDescuento4;
     private javax.swing.JTextField txtSueldo;
+    private javax.swing.JTextField txtTotal;
+    private javax.swing.JTextField txtdescuento1;
+    private javax.swing.JTextField txtdescuento2;
     // End of variables declaration//GEN-END:variables
 }
